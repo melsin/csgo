1c5f5328a368515ce7c631dba67c8d75150f5f0f
melsin <mel.is.not@gmail.com>
2020-04-22 16:45:58 +1000

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -2,4 +2,6 @@
 
 $ {STEAMCMDDIR} /steamcmd.sh + login anonymous + force_install_dir $ {STEAMAPPDIR} + app_update $ {STEAMAPPID} + quit
 
-$ {STEAMAPPDIR} / srcds_run -game csgo -console -autoupdate $ SRCDS_CMD
+$ {STEAMAPPDIR} / srcds_run -game csgo -console -autoupdate -steam_dir $ {STEAMCMDDIR} -steamcmd_script $ {STEAMAPPDIR} /csgo_update.txt -usercon + fps_max $ SRCDS_FPSMAX \
+			-tickrate $ SRCDS_TICKRATE -maxplayers_override $ SRCDS_MAXPLAYERS + game_type $ SRCDS_GAMETYPE + game_mode $ SRCDS_GAMEMODE \
+			+ mapgroup $ SRCDS_MAPGROUP + map $ SRCDS_STARTMAP + sv_setsteamaccount $ SRCDS_TOKEN + rcon_password $ SRCDS_RCONPW + sv_region $ SRCDS_REGION
