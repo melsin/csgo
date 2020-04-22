9c13d6bb7dc3a994cd7594ce8d3829bcc8992ba3
melsin <mel.is.not@gmail.com>
2020-04-22 13:51:19 +1000

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,3 +1,5 @@
 #! / bin / bash
 
 $ {STEAMCMDDIR} /steamcmd.sh + login anonymous + force_install_dir $ {STEAMAPPDIR} + app_update $ {STEAMAPPID} + quit
+
+$ {STEAMAPPDIR} / srcds_run -game csgo -console -autoupdate $ SRCDS_CMD
